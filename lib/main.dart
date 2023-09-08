61e43244e00cd1f0b338bdff8e6295310ed153a1
abdelaziz <abdelaziz.mdidech@gmail.com>
2023-09-08 12:58:51 +0100

    add column, SizedBox and SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -52,30 +52,83 @@ class FacebookApp extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
-      body: Container(
-          // color: Colors.amber,
-          margin: EdgeInsets.symmetric(vertical: 35, horizontal: 16),
-          padding: EdgeInsets.all(20),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-              // border: Border.all(color: Colors.orange, width: 5),
-              color: Colors.blueGrey,
-              // borderRadius: BorderRadius.circular(10),
-              shape: BoxShape.circle),
-          child: Text(
-            "M'didech Abdelaziz",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-              fontStyle: FontStyle.italic,
-              fontWeight: FontWeight.w500,
-            ),
+      body: SingleChildScrollView(
+        child: Column(
+            // mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  // color: Colors.amber,
+                  // margin: EdgeInsets.symmetric(vertical: 35, horizontal: 16),
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      // border: Border.all(color: Colors.orange, width: 5),
+                      color: Colors.blueGrey,
+                      // borderRadius: BorderRadius.circular(10),
+                      shape: BoxShape.circle),
+                  width: 200,
+                  height: 200,
+                  child: Text(
+                    "M'didech Abdelaziz",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
 
-            // maxLines: 2,
-            overflow: TextOverflow.visible,
-            textAlign: TextAlign.left,
-            textDirection: TextDirection.rtl,
-          )),
+                    // maxLines: 2,
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.left,
+                    textDirection: TextDirection.rtl,
+                  )),
+              Container(
+                  // color: Colors.amber,
+                  // margin: EdgeInsets.symmetric(vertical: 35, horizontal: 16),
+                  padding: EdgeInsets.all(20),
+                  margin: EdgeInsets.only(top: 400),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      // border: Border.all(color: Colors.orange, width: 5),
+                      color: Colors.blueGrey,
+                      // borderRadius: BorderRadius.circular(10),
+                      shape: BoxShape.circle),
+                  width: 200,
+                  height: 200,
+                  child: Text(
+                    "FrontEnd Developer",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w500,
+                    ),
+
+                    // maxLines: 2,
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.left,
+                    textDirection: TextDirection.rtl,
+                  )),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "UX/UI Design",
+                style: TextStyle(backgroundColor: Colors.teal, fontSize: 33),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite),
+                color: Colors.red,
+                iconSize: 50,
+                // alignment: Alignment.topLeft,
+              )
+            ]),
+      ),
     );
   }
 }
